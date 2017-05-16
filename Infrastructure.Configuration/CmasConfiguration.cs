192182fb681f2bf9e937fd49fb59301d633a1037
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-16 16:17:31 +0300

    CMAS-389 Создать конфиг на backend'e

--- Infrastructure.Configuration/CmasConfiguration.cs
+++ Infrastructure.Configuration/CmasConfiguration.cs
@@ -1,17 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Cmas.Infrastructure.Configuration
-{ 
+{
     public class CmasConfiguration
     {
-        public CmasConfiguration()
+        public static string secretKey = @"P546C8TF278CH5931069B622E695D4F4";
+
+        public Databases Databases { get; set; }
+
+        public CORS CORS { get; set; }
+
+        public Database GetDbSettings(string serviceName)
         {
-            Option1 = "value1_from_ctor";
+            serviceName = serviceName.ToLower();
+
+            Database result = null;
+
+            switch (serviceName)
+            {
+                case "users":
+                    result = Databases.Users;
+                    break;
+                case "call-off-orders":
+                    result = Databases.CallOffOrders;
+                    break;
+                case "contracts":
+                    result = Databases.Contracts;
+                    break;
+                case "requests":
+                    result = Databases.Requests;
+                    break;
+                case "time-sheets":
+                    result = Databases.TimeSheets;
+                    break;
+                default:
+                    throw new Exception($"unknown service: {serviceName}");
+            }
+
+            return result.WithDefaults(Databases.Schema, Databases.Host, Databases.Login, Databases.Password);
         }
 
-        public string Option1 { get; set; }
-        public int Option2 { get; set; } = 5;
+        public Smtp Smtp { get; set; }
+
+        public string CmasUrl { get; set; }
+    }
+
+    public class Database
+    {
+       
+
+        public string Schema { get; set; }
+        public string Host { get; set; }
+        public string Login { get; set; }
+
+        private string cryptedPassword;
+
+        public string Password
+        {
+            get { return ConfigurationHelper.DecryptString(cryptedPassword, CmasConfiguration.secretKey); }
+            set { cryptedPassword = value; }
+        }
+
+        public string Name { get; set; }
+
+        public string ConnectionString
+        {
+            get { return $"{Schema}://{Login}:{Password}@{Host}"; }
+        }
+    }
+
+    public class Databases
+    {
+        public string Schema { get; set; }
+        public string Host { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public Database Users { get; set; }
+        public Database CallOffOrders { get; set; }
+        public Database Contracts { get; set; }
+        public Database Requests { get; set; }
+        public Database TimeSheets { get; set; }
+    }
+
+    public class CORS
+    {
+        public string Origin { get; set; }
+        public string Methods { get; set; }
+        public string Headers { get; set; }
+    }
+
+    public class Smtp{
+        public string From { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Login { get; set; }
+
+        private string cryptedPassword;
+
+        public string Password
+        {
+            get { return ConfigurationHelper.DecryptString(cryptedPassword, CmasConfiguration.secretKey); }
+            set { cryptedPassword = value; }
+        }
     }
 }
---
+++ Infrastructure.Configuration/ConfigurationHelper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Cmas.Infrastructure.Configuration
+{
+    public static class ConfigurationHelper
+    {
+        public static CmasConfiguration GetConfiguration(this IServiceProvider serviceProvider)
+        {
+            return ((IOptions<CmasConfiguration>) serviceProvider.GetService(typeof(IOptions<CmasConfiguration>))).Value;
+        }
+
+        public static string EncryptString(string text, string keyString)
+        {
+            var key = Encoding.UTF8.GetBytes(keyString);
+
+            using (var aesAlg = Aes.Create())
+            {
+                using (var encryptor = aesAlg.CreateEncryptor(key, aesAlg.IV))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+
+                        var iv = aesAlg.IV;
+
+                        var decryptedContent = msEncrypt.ToArray();
+
+                        var result = new byte[iv.Length + decryptedContent.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText, string keyString)
+        {
+            if (string.IsNullOrEmpty(cipherText) || string.IsNullOrEmpty(keyString))
+                return null;
+
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            var iv = new byte[16];
+            var cipher = new byte[16];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
+            var key = Encoding.UTF8.GetBytes(keyString);
+
+            using (var aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(key, iv))
+                {
+                    string result;
+                    using (var msDecrypt = new MemoryStream(cipher))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                result = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+            }
+        }
+
+        public static Database WithDefaults(this Database database, string schema, string host, string login, string password)
+        {
+            if (string.IsNullOrEmpty(database.Schema))
+            {
+                database.Schema = schema;
+            }
+
+            if (string.IsNullOrEmpty(database.Host))
+            {
+                database.Host = host;
+            }
+
+            if (string.IsNullOrEmpty(database.Login))
+            {
+                database.Login = login;
+            }
+
+            if (string.IsNullOrEmpty(database.Password))
+            {
+                database.Password = password;
+            }
+
+            return database;
+
+        }
+    }
+}
